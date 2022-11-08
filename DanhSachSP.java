9463df41cfd5987dac1107cacc5b65759c44f54a
huuphuoc <huuphuoc0162@gmail.com>
2022-11-08 21:11:25 +0700

    ds

--- DanhSachSP.java
+++ DanhSachSP.java
@@ -1,6 +1,7 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
-public class DanhSachSP {
+public class DanhSachSP implements ChucNangDS {
     private int n;
     SanPham dssp[];
     Scanner sc = new Scanner(System.in);
@@ -10,7 +11,7 @@ public class DanhSachSP {
         dssp = null;
     }
 
-    void them() {
+    void nhapds() {
         System.out.println("Nhap vao so luong san pham can them: ");
         n = Integer.parseInt(sc.nextLine());
         dssp = new SanPham[n];
@@ -22,17 +23,126 @@ public class DanhSachSP {
         }
     }
 
-    void hienThi() {
-        System.out.println("Danh sach san pham vua nhap");
-        System.out.printf("%-25s%-25s%-25s%-25s%-25s\n", "[MaSP]", "[TenSP]", "[SoluongSP]", "[GiaSP]", "[ThongtinSP]");
-        for(int i=0;i<dssp.length;i++){
-            dssp[i].Xuat();
+    public void them() {
+        System.out.println("Nhap vao thong tin SP ban can them: ");
+        SanPham addsp = new SanPham();
+        addsp.Nhap();
+        dssp = Arrays.copyOf(dssp, dssp.length + 1);
+        dssp[n] = addsp;
+        n++;
+    }
+
+    public void timKiemTheo_MaSP() {
+        System.out.println("Nhap vao ma SP ban muon tim: ");
+        String masp = sc.nextLine();
+        for (int i = 0; i < dssp.length; i++) {
+            if (dssp[i].getMasp().equalsIgnoreCase(masp)) {
+                dssp[i].Xuat();
+            }
         }
     }
 
-    public static void main(String[] args) {
-        DanhSachSP ds = new DanhSachSP();
-        ds.them();
-        ds.hienThi();
+    public void timKiemTheo_TenSP() {
+        System.out.println("Nhap vao ten SP ma ban muon tim: ");
+        String tensp = sc.nextLine();
+        for (int i = 0; i < dssp.length; i++) {
+            if (dssp[i].getTensp().equalsIgnoreCase(tensp)) {
+                dssp[i].Xuat();
+            }
+        }
+    }
+
+    public void timKiemTheo_GiaSP() {
+        System.out.println("Nhap vao gia SP ban muon tim: ");
+        String giasp = sc.nextLine();
+        for (int i = 0; i < dssp.length; i++) {
+            if (dssp[i].getGiasp().equalsIgnoreCase(giasp)) {
+                dssp[i].Xuat();
+            }
+        }
+    }
+
+    public void timKiem() {
+        int luachon;
+        System.out.println("1.tim kiem theo ma SP");
+        System.out.println("2.tim kiem theo ten SP");
+        System.out.println("3.tim kiem theo gia SP");
+        System.out.println("4.Thoat!");
+        do{
+            System.out.println("Nhap vao lua chon cua ban: ");
+            luachon = Integer.parseInt(sc.nextLine());
+            switch (luachon) {
+                case 1:
+                    timKiemTheo_MaSP();
+                    break;
+                case 2:
+                    timKiemTheo_TenSP();
+                    break;
+                case 3:
+                    timKiemTheo_GiaSP();
+                    break;
+                default:
+                    System.out.println("Moi ban nhap lai lua chon: ");
+                    break;
+            }
+        }while(luachon != 4);
+    }
+
+    public void suaTheo_GiaSP() {
+        System.out.println("nhap vao ten san pham ban muon sua gia: ");
+        String tensp = sc.nextLine();
+        System.out.println("Nhap vao gia ban muon sua: ");
+        String giasp = sc.nextLine();
+        for (int i = 0; i < dssp.length; i++) {
+            if (dssp[i].getTensp().equalsIgnoreCase(tensp)) {
+                dssp[i].Xuat();
+                dssp[i].setGiasp(giasp);
+                dssp[i].Xuat();
+            }
+        }
+    }
+
+    public void suaTheo_ThongTinSP(){
+        System.out.println("nhap vao ten san pham ban muon sua gia: ");
+        String tensp = sc.nextLine();
+        System.out.println("Nhap vao gia ban muon sua: ");
+        String thongtinsp = sc.nextLine();
+        for(int i=0;i<dssp.length;i++){
+            if(dssp[i].getTensp().equalsIgnoreCase(tensp)){
+                dssp[i].Xuat();
+                dssp[i].setThongtinsp(thongtinsp);
+                dssp[i].Xuat();
+            }
+        }
+    }
+
+    public void sua() {
+        int luachon;
+        System.out.println("1.Sua gia SP");
+        System.out.println("1.Sua thong tin SP");
+        System.out.println("3.Thoat!");
+        do {
+            System.out.println("nhap vao lua chon cua ban: ");
+            luachon = Integer.parseInt(sc.nextLine()); 
+            switch (luachon) {
+                case 1:
+                    suaTheo_GiaSP();
+                    break;
+                case 2:
+                    suaTheo_ThongTinSP();
+                    break;
+                default:
+                    System.out.println("Moi ban nhap lai lua chon!");
+                    break;
+            }
+        } while (luachon != 3);
+    }
+
+    public void hienThi() {
+        System.out.println("Danh sach san pham vua nhap");
+        System.out.printf("%-25s%-25s%-25s%-25s%-25s\n", "[MaSP]", "[TenSP]", "[SoluongSP]", "[GiaSP]", "[ThongtinSP]");
+        for (int i = 0; i < dssp.length; i++) {
+            dssp[i].Xuat();
+        }
     }
 }
