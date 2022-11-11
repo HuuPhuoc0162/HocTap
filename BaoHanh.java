667ac6762593895c53aa395a1c1fffded83fafe6
huuphuoc <huuphuoc0162@gmail.com>
2022-11-12 02:40:38 +0700

    add

--- BaoHanh.java
+++ BaoHanh.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
+
 public class BaoHanh {
     private int mabh;
     private String thoigianbh;
     private String thongtinbh;
 
-    public BaoHanh() {
+    Scanner sc = new Scanner(System.in);
 
+    public BaoHanh() {
+        this.mabh = 0;
+        this.thoigianbh = null;
+        this.thongtinbh = null;
     }
 
     public BaoHanh(int mabh, String thoigianbh, String thongtinbh) {
@@ -35,5 +41,18 @@ public class BaoHanh {
 
     public void setThongtinbh(String thongtinbh) {
         this.thongtinbh = thongtinbh;
+    }
+
+    public void Nhap() {
+        System.out.println("Nhap vao ma bao hanh: ");
+        mabh = Integer.parseInt(sc.nextLine());
+        System.out.println("Nhap vao thoi gian bao hanh: ");
+        thoigianbh = sc.nextLine();
+        System.out.println("Nhap vao thong tin bao hanh: ");
+        thongtinbh = sc.nextLine();
+    }
+
+    public void Xuat() {
+        System.out.printf("%-25d%-25s%-25s\n", mabh, thoigianbh, thongtinbh);
     }
 }
